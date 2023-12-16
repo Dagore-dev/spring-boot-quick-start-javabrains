ed2096491c46898890c16616044582f9b71eef13
Dagore <david.gomez.redondo.dev@gmail.com>
2023-12-16 20:38:51 +0100

    derbyDB && CrudRepository

--- src/main/java/dev/dagore/springbootquickstart/topic/TopicService.java
+++ src/main/java/dev/dagore/springbootquickstart/topic/TopicService.java
@@ -1,41 +1,38 @@
 package dev.dagore.springbootquickstart.topic;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-  private List<Topic> topicsList = new ArrayList<>(Arrays.asList(
-      new Topic("spring", "Spring Framework", "Spring Framework description"),
-      new Topic("java", "Core java", "Core java description"),
-      new Topic("javascript", "Javascript", "Javascript description")));
+  @Autowired
+  private TopicRepository topicRepository;
 
   public List<Topic> getAllTopics() {
+    List<Topic> topicsList = new ArrayList<>();
+    topicRepository
+        .findAll()
+        .forEach(topicsList::add);
+
     return topicsList;
   }
 
   public Topic getTopicById(String id) {
-    return topicsList.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
+    return topicRepository.findById(id).orElseThrow();
   }
 
   public void addTopic(Topic topic) {
-    topicsList.add(topic);
+    topicRepository.save(topic);
   }
 
   public void updateTopic(String id, Topic topic) {
-    for (int i = 0; i < topicsList.size(); i++) {
-      Topic t = topicsList.get(i);
-      if (t.getId().equals(topic.getId())) {
-        topicsList.set(i, topic);
-        return;
-      }
-    }
+    topicRepository.save(topic);
   }
 
   public void deleteTopic(String id) {
-    topicsList.removeIf(t -> t.getId().equals(id));
+    topicRepository.deleteById(id);
   }
 }
